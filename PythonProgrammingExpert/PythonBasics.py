b9cacf13e30036e83c2a91ed2300ce1912c49031
geekyWomaniya <geekywomaniya@gmail.com>
2022-06-16 12:05:36 -0500

    Python Fundamentals 1-6

---
+++ PythonProgrammingExpert/PythonBasics.py
@@ -0,0 +1,53 @@
+#16June- Python Programming Fundamentals_1
+# 1) Intro 2) Data Types 3) Comments
+# 4) Variables & Printing 5) Console Input 6) Arithmatic Operators 
+
+##################################INTRO##################################
+"""
+Python is general purpose purpose programming and scripting language, that is easy to learn and provides 
+simple yet powerful syntax. Used for- AI/ML, Data Science, Web Development, etc.
+"""
+##############################DATA TYPES##################################
+"""
+Int: ...,-2,-1,0,1,2,....
+Float: ....,-2.0,-1.4,0,1.87,2.89,...
+String: "hey", "5.0", 'hello world', 'true'
+Boolean: True, False
+"""
+
+##############################COMMENTS##################################
+#Single line comment
+'''
+Multiline
+Comments
+'''
+##############################Variables & Printing##################################
+var_1 = "That's my first variable- var_1"
+print(var_1)
+
+_var = var1 = var_1 = vAr_1 = var_ = "All of these are valid variables"
+
+##############################Console & Input##################################
+#my_name = input("Please input your name: ")
+#print("And that's your name-", my_name, "!!!")
+
+##############################Arithmatic Operators#############################
+num1 = 8
+num2 = 3
+adds = num1 + num2
+subs = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+modulus = num1 % num2
+integer_division = num1 // num2
+print("Addition: ", adds)
+print("Subtraction:", subs)
+print("Multiplication:", mul)
+print("Division: ", div)
+print("Modulus: ",modulus)
+print("Integer Division- Divides and discards the fractional part:\n", integer_division)
+
+
+
+
+
