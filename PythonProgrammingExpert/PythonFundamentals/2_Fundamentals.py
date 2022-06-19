1d87da4e331afe04bca2928bd506014274a1def0
geekyWomaniya <geekywomaniya@gmail.com>
2022-06-19 16:41:45 -0500

    Updating Resume

---
+++ PythonProgrammingExpert/PythonFundamentals/2_Fundamentals.py
@@ -0,0 +1,17 @@
+#17June- Python Programming Fundamentals_2
+#7) Type Conversions 8) Conditions 9) Compound Conditions
+#10) Conditionals 11) Lists 12) Strings
+
+#################################TYPE CONVERSIONS##################################
+num1 = input("Enter the number:")
+type(num1) #str
+#input by default takes string, therefore explicitly convert it to int
+n1 = int(num1)
+type(n1) #int
+
+bool_value = bool("Saba Ruhee")
+print(bool_value) #True
+
+int(10.8) #True
+
+#################################CONDITIONS##################################
